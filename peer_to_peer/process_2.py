006b6a5c17b3d12d5631681eb0e8f221a7c77524
rachiebytes <racheljohnson457@gmail.com>
2020-09-13 19:57:26 -0700

    add TODOs

--- peer_to_peer/process_2.py
+++ peer_to_peer/process_2.py
@@ -2,4 +2,10 @@ from chat import ChatApp
 
 
 if __name__ == "__main__":
-    ChatApp("127.0.0.1", 4442, "127.0.0.1", 4441)
+    local_host = "127.0.0.1"
+    local_port = # TODO
+
+    remote_host = "127.0.0.1"
+    remote_port = # TODO
+
+    ChatApp(local_host, local_port, remote_host, remote_port)
