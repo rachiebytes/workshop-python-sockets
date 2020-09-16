f120694963eb2e934536688f45fcc4cbaf24007f
rachiebytes <racheljohnson457@gmail.com>
2020-09-15 18:32:19 -0700

    remove ports

--- peer_to_peer/process_2.py
+++ peer_to_peer/process_2.py
@@ -3,9 +3,9 @@ from chat import ChatApp
 
 if __name__ == "__main__":
     local_host = "127.0.0.1"
-    local_port = 12346
+    local_port = # TODO
 
     remote_host = "127.0.0.1"
-    remote_port = 12345
+    remote_port = # TODO
 
     ChatApp(local_host, local_port, remote_host, remote_port)
